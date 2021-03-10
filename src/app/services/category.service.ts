478ec6f5e5a7c4ce165caf6f073b2ec3121d12f6
berkbasarir <bbasarir@gmail.com>
2021-03-11 01:55:22 +0300

    10.03.2021 - Lesson17

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryResponseModel } from '../models/categoryResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  apiUrl = 'https://localhost:44365/api/categories/getall';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getCategories(): Observable<CategoryResponseModel> {
+    return this.httpClient.get<CategoryResponseModel>(this.apiUrl);
+  }
+}
